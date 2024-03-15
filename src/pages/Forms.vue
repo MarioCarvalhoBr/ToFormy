ca2614fbd3e06548ab7063e194952ab482190e47
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-03-14 21:53:56 -0400

    Add surveyjs Form Library

--- src/pages/Forms.vue
+++ src/pages/Forms.vue
@@ -1,64 +1,28 @@
-<!-- components/SurveyCreator.vue -->
 <script setup lang="ts">
 import 'survey-core/defaultV2.min.css';
-import "survey-creator-core/survey-creator-core.min.css";
+import { Model } from 'survey-core';
 
-import type { ICreatorOptions } from "survey-creator-core";
-import { SurveyCreatorModel } from "survey-creator-core";
-
-const creatorOptions: ICreatorOptions = {
-  showLogicTab: true,
-  isAutoSave: true
-};
-
-const defaultJson = {
-  pages: [{
-    name: "Name",
-    elements: [{
-      name: "FirstName",
-      title: "Enter your first name:",
-      type: "text"
-    }, {
-      name: "LastName",
-      title: "Enter your last name:",
-      type: "text"
-    }]
+const surveyJson = {
+  elements: [{
+    name: "FirstName",
+    title: "Enter your first name:",
+    type: "text"
+  }, {
+    name: "LastName",
+    title: "Enter your last name:",
+    type: "text"
   }]
 };
-const creator = new SurveyCreatorModel(creatorOptions);
-creator.text = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
-creator.saveSurveyFunc = (saveNo: number, callback: Function) => {
-  window.localStorage.setItem("survey-json", creator.text);
-  callback(saveNo, true);
-  // saveSurveyJson(
-  //     "https://your-web-service.com/",
-  //     creator.JSON,
-  //     saveNo,
-  //     callback
-  // );
-};
 
-// function saveSurveyJson(url: string, json: object, saveNo: number, callback: Function) {
-//   fetch(url, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json;charset=UTF-8'
-//     },
-//     body: JSON.stringify(json)
-//   })
-//   .then(response => {
-//     if (response.ok) {
-//       callback(saveNo, true);
-//     } else {
-//       callback(saveNo, false);
-//     }
-//   })
-//   .catch(error => {
-//     callback(saveNo, false);
-//   });
-// }
+const alertResults = (sender: any) => {
+  const results = JSON.stringify(sender.data);
+  alert(results);
+}
+
+const survey = new Model(surveyJson);
+survey.onComplete.add(alertResults);
 </script>
 
 <template>
-  <SurveyCreatorComponent :model="creator" />
+  <SurveyComponent :model="survey" />
 </template>

1685dcedc6f4a2bf72388aa071631ede0cf90e2e
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2024-03-14 21:57:20 -0400

    Update Forms.vue with new surveyJson

--- src/pages/Forms.vue
+++ src/pages/Forms.vue
@@ -3,16 +3,50 @@ import 'survey-core/defaultV2.min.css';
 import { Model } from 'survey-core';
 
 const surveyJson = {
-  elements: [{
-    name: "FirstName",
-    title: "Enter your first name:",
-    type: "text"
-  }, {
-    name: "LastName",
-    title: "Enter your last name:",
-    type: "text"
-  }]
-};
+    "locale": "pt-br",
+    "title": "ToFormy",
+    "description": "O projeto ToFormy é um projeto de código aberto que visa ajudar as pessoas a criar formulários de maneira fácil e rápida.",
+    "completedHtml": {
+     "default": "<h3>Formulário finalizado!</h3>",
+    },
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "text",
+        "name": "question1"
+       }
+      ],
+      "title": "Info"
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "text",
+        "name": "question2"
+       }
+      ],
+      "title": "Address"
+     },
+     {
+      "name": "page3",
+      "elements": [
+       {
+        "type": "text",
+        "name": "question3"
+       }
+      ],
+      "title": "Contact"
+     }
+    ],
+    "showPageNumbers": true,
+    "showQuestionNumbers": "off",
+    "showProgressBar": "bottom",
+    "progressBarType": "pages",
+    "mode": "edit"
+   };
 
 const alertResults = (sender: any) => {
   const results = JSON.stringify(sender.data);
